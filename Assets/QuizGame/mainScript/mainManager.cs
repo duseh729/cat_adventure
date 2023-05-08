bc9c71f7073c9c3ff409de6e6218512b8574646d
DoYeon Ahn <duseh729@naver.com>
2023-05-08 21:29:20 +0900

    feat: Esc모달창 및 게임 일시정지 구현(팩맨에 안보임)

--- Assets/QuizGame/mainScript/mainManager.cs
+++ Assets/QuizGame/mainScript/mainManager.cs
@@ -5,10 +5,19 @@ using UnityEngine.UI;
 
 public class mainManager : MonoBehaviour
 {
-    public GameObject easyButton;
-    public GameObject basicButton;
-    public GameObject hardButton;
-
-    void Start() {
+    public GameObject escModal;
+    public static bool escFlag = false;
+    void Update() {            
+        escModal.SetActive(escFlag);
+        // esc 플래그에 따른 모달 컨트롤
+        if (Input.GetKeyDown(KeyCode.Escape) && escFlag==false){
+            // 게임 일시정지 하기
+            Time.timeScale = 0f;
+            escFlag = true;
+            Debug.Log("esc 눌렸노");
+        }else if(Input.GetKeyDown(KeyCode.Escape) && escFlag!=false) {
+            Time.timeScale = 1f;
+            escFlag = false;
+        }
     }
 }
